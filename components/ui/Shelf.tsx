0dd3d9bea47b561e3f636cd6091e2a168babaafb
ian <ian@MacBook-Pro.local>
2020-05-18 20:46:36 +0200

    shelf fix

--- components/ui/Shelf.tsx
+++ components/ui/Shelf.tsx
@@ -4,35 +4,25 @@ import { animated, useSpring } from "react-spring";
 import { RemoveScroll } from "react-remove-scroll";
 import { useWindowDimensions } from "../../hooks/useWindowDimensions";
 import { theme } from "../../Theme";
+import { useIsMobile } from "../../hooks/useIsMobile";
 
 interface Shelf {
   isVisible: boolean;
-  width?: string;
 }
 
-export const Shelf: React.FC<Shelf> = ({
-  isVisible,
-  width,
-  children,
-  ...rest
-}) => {
-  const { width: screenWidth } = useWindowDimensions();
-  const { screenSizes } = theme;
+export const Shelf: React.FC<Shelf> = ({ isVisible, children, ...rest }) => {
+  const { isMobile } = useIsMobile();
 
   const styles = useSpring({
     transform: `translateX(${isVisible ? 0 : "100%"})`,
   });
-
-  React.useEffect(() => {
-    width = screenWidth > screenSizes.TABLET ? "30vw" : "100vw";
-  }, [screenWidth]);
 
   return (
     <RemoveScroll enabled={isVisible}>
       <animated.div
         style={styles}
         css={{
-          width,
+          width: isMobile ? "100vw" : 400,
           height: "100vh",
           backgroundColor: "#000",
           position: "fixed",
